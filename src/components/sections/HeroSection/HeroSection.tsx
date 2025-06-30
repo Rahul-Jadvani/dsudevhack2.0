01cf507492339dfd0671f562e5993f77fc5962ee
Rahul Jadvani <rahuljadvani24@gmail.com>
2025-07-01 03:38:18 +0530

    desktop fix

--- src/components/sections/HeroSection/HeroSection.tsx
+++ src/components/sections/HeroSection/HeroSection.tsx
@@ -188,7 +188,7 @@ export const HeroSection = (_props: HeroSectionProps) => {
                 ))}
               </div>
               <div 
-                className="apply-button devfolio-button mx-auto"
+                className="apply-button devfolio-button mx-auto ml-16"
                 data-hackathon-slug="dsudevhack2" 
                 data-button-theme="dark"
               ></div>
