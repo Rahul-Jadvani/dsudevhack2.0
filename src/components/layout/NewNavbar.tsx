b569fadb26b609451e675e4346115e3472eebbcd
ManaviP <manavimunnuraya11@gmail.com>
2025-07-17 18:48:01 +0530

    mlh badge update

--- src/components/layout/NewNavbar.tsx
+++ src/components/layout/NewNavbar.tsx
@@ -5,17 +5,17 @@ import './NewNavbar.css';
 export const NewNavbar = () => {
   const MLHBadge = () => (
     <a
-      id="mlh-trust-badge"
-      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-white.svg"
-        alt="Major League Hacking 2026 Hackathon Season"
-        style={{ width: '100%' }}
-      />
-    </a>
+    id="mlh-trust-badge"
+    href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=red"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-red.svg"
+      alt="Major League Hacking 2026 Hackathon Season"
+      style={{ width: '100%' }}
+    />
+  </a>  
   );
   
 
