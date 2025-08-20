04573b5081803a205f73f4e5cad6d2f946d9c1d0
ManaviP <manavimunnuraya11@gmail.com>
2025-08-20 21:59:28 +0530

    title update

--- src/components/sections/HeroSection/HeroSection.tsx
+++ src/components/sections/HeroSection/HeroSection.tsx
@@ -235,7 +235,7 @@ export const HeroSection = (_props: HeroSectionProps) => {
            <div className="max-w-full w-full sm:w-[600px] sm:p-6 bg-transparent rounded-xl transition-transform duration-300 hover:-translate-y-1 relative overflow-visible flex flex-col items-center justify-center mt-16 sm:mt-0">
   <div className="flex flex-col items-center max-w-full overflow-visible">
     <div className="flex flex-col items-center max-w-full overflow-hidden pt-12">
-    <div className="w-full flex justify-center md:justify-start absolute top-8 md:top-2 md:-right-11 pb-6 sm:pb-0 sm:p-2 max-w-[500px]">
+    <div className="w-full flex justify-center md:justify-start absolute top-8 md:top-4 md:-right-11 pb-6 sm:pb-0 sm:p-2 max-w-[500px]">
   <img
     src="/images/vultr1.png"
     alt="Vultr Logo"
